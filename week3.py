1b4d522644f0d33a9cbea13b013f9c5964a2c014
k8hertweck <k8hertweck@gmail.com>
2019-05-21 17:05:50 -0700

    moving errors to week 3

--- week3.py
+++ week3.py
@@ -10,6 +10,7 @@
 #   including help documentation within a function
 #   defining defaults for a function
 #   applying readable code convention to functions
+#   identify and correct common errors reported by Python
 
 ## Challenge: what will be printed if you run this code?
 a = 3
@@ -190,6 +191,99 @@ def std_dev(sample):
 #   blank spaces
 
 ## Challenge: Revise a function you wrote for one of the previous exercises to try to make the code more readable. Then, collaborate with one of your neighbors to critique each other’s functions and discuss how your function implementations could be further improved to make them more readable.
+
+#### Errors and Exceptions ####
+
+# example traceback: execute all at once
+def favorite_ice_cream():
+    ice_creams = [
+        "chocolate",
+        "vanilla",
+        "strawberry"
+    ]
+    print(ice_creams[3])
+
+favorite_ice_cream()
+
+# interpreting traceback:
+#   determine number of levels by looking for arrows on lefthand side
+#   first arrow points to line 8, favorite_ice_cream()
+#   second arrow shows code within function, line 6
+#   last level shown is where error occurred
+#   most of the time you can pay attention to last level!
+
+# three main types of errors
+
+## Syntax errors
+
+# error from defining a function incorrectly
+def some_function()
+    msg = "hello, world!"
+    print(msg)
+     return msg
+# colon error: add colon to first line
+def some_function():
+    msg = "hello, world!"
+    print(msg)
+     return msg
+# another error! IntentationError is a type of syntax, but is always about indentation
+# correct indentation:
+def some_function():
+    msg = "hello, world!"
+    print(msg)
+    return msg
+# whitespace: tabs and spaces
+# many interpreters correct spaces meant to be tabs, but python doesn't allow you to mix!
+
+## Variable name errors
+
+# try to print a variable
+print(octopus)
+# NameErrors can be difficult to fix
+# common problem is that it's not a variable, but a string you forgot to place quotations around
+print("octopus")
+# another problem: forgot to create variable before using it:
+#count = 0 # add this to fix it
+for number in range(10):
+    count = count + number
+print("The count is:", count)
+# if you accidentally include Count = 0 instead, this will also give variable name error!
+
+## Index errors
+# accessing an item in a container that doesn't exist
+letters = ['a', 'b', 'c']
+print("Letter #1 is", letters[0])
+print("Letter #2 is", letters[1])
+print("Letter #3 is", letters[2])
+print("Letter #4 is", letters[3])
+
+## File errors
+
+# trying to read a file that doesn't exist, or is in a different location
+file_handle = open('myfile.txt')
+file_handle = open("inflammation-01.csv", 'r')
+# attempt to write to a file that was opened read-only: UnsupportedOperationError
+# also IOErrors or OSErrors, depending on the version of Python
+
+## Challenge:
+# This code has an intentional error. Do not type it directly;
+# use it for reference to understand the error message below.
+def print_message(day):
+    messages = {
+        "monday": "Hello, world!",
+        "tuesday": "Today is tuesday!",
+        "wednesday": "It is the middle of the week.",
+        "thursday": "Today is Donnerstag in German!",
+        "friday": "Last day of the week!",
+        "saturday": "Hooray for the weekend!",
+        "sunday": "Aw, the weekend is almost over."
+    }
+    print(messages[day])
+
+def print_friday_message():
+    print_message("Friday")
+
+print_friday_message()
 
 #### Wrapping up ####
 
